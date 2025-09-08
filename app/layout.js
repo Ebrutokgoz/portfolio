3cdf193d85695244ebb3527ffd21810050f965e9
ebrutokgoz <ebrutz44@gmail.com>
2025-09-08 21:39:42 +0300

    Create hero section

--- app/layout.js
+++ app/layout.js
@@ -21,7 +21,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${outfit.variable} ${ovo.variable} antialiased`}>{children}</body>
+      <body className={`${outfit.className} ${ovo.className}  leading-8 overflow-x-hidden`}>{children}</body>
     </html>
   );
 }
