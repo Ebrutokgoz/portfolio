91d66762cab05c22723acf6db82548ef54b79a99
ebrutokgoz <ebrutz44@gmail.com>
2025-09-09 17:23:40 +0300

    Create works section

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import AboutMe from "./components/AboutMe";
 import Services from "./components/Services";
+import Works from "./components/Works";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <Hero />
       <AboutMe />
       <Services />
+      <Works />
     </>
   );
 }
