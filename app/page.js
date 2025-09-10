feadde17eeddc8a6120c2176fa32352945a8859a
ebrutokgoz <ebrutz44@gmail.com>
2025-09-10 11:22:11 +0300

    Create contact section

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ import Hero from "./components/Hero";
 import AboutMe from "./components/AboutMe";
 import Services from "./components/Services";
 import Works from "./components/Works";
+import Contact from "./components/Contact";
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
       <AboutMe />
       <Services />
       <Works />
+      <Contact />
     </>
   );
 }
